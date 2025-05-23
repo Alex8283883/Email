e9a0b728e4e69c5f1e17c0c791df5eaf5c947b5b
Alex8283883 <89088281+Alex8283883@users.noreply.github.com>
2025-05-23 17:04:49 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [email, setEmail] = useState("");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch("/api/latest-email")
+        .then(res => res.json())
+        .then(data => setEmail(data.body || "No email yet"));
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Latest Email from SkinApe</h1>
+      <pre>{email}</pre>
+    </div>
+  );
+}
